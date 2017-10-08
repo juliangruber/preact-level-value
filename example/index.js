37ee7f15eab66fb3defcc3ebae0222e64e895e52
Julian Gruber <julian@juliangruber.com>
2017-10-08 23:03:36 +0200

    make it work

--- example/index.js
+++ example/index.js
@@ -2,18 +2,38 @@ import { h, render, Component } from 'preact'
 import level from 'level'
 
 const db = level('/tmp/level-react')
+window.db = db
 
-class List extends Component {
-  componentDidMount () {
-    console.log('did mount')
+class Example extends Component {
+  constructor () {
+    super()
+    this.state = { value: '' }
+    this._update = this.update.bind(this)
   }
+
+  update (key, value) {
+    if (key === 'key') this.setState({ value: value })
+  }
+
+  componentDidMount () {
+    db.on('put', this._update)
+    db.get('key', (err, value) => {
+      if (err) throw err
+      this._update('key', value)
+    })
+  }
+
+  componentWillUnmount () {
+    db.removeListener('put', this._update)
+  }
+
   render () {
-    return
-      <div id="foo">
-          <span>Hello, world!</span>
-          <button onClick={ e => alert("hi!") }>Click Me</button>
+    return <div id="example">
+        <ul>
+          <li>Value: <strong>{this.state.value}</strong></li>
+        </ul>
       </div>
   }
 }
 
-render(<List />, document.body)
+render(<Example />, document.body)

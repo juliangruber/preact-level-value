4c39dafafb67d292be677c31ce19d749aca60099
Julian Gruber <julian@juliangruber.com>
2017-10-10 17:48:21 +0200

    modernize example

--- example/index.js
+++ example/index.js
@@ -6,14 +6,14 @@ const db = window.db = level('/tmp/preact-level-value')
 
 class Example extends Component {
   random () {
-    db.put('key', Math.random(), err => err && console.error(err))
+    db.put('key', Math.random())
   }
   render () {
     return <div id="example">
         Push this button:<br />
         <button onclick={ this.random }>Random!</button><br /><br />
         Or execute this snippet in the console:<br />
-        <pre>db.put('key', Math.random(), err => err && console.error(err))</pre><br />
+        <pre>db.put('key', Math.random())</pre><br />
         Live value: <strong><Value db={db} key="key" /></strong>
       </div>
   }
